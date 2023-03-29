f384b7d4be9a4f526e548ba8de5e73dcb08f59e5
Sydpaltra <paltra@tu-berlin.de>
2023-03-29 14:59:59 +0200

    Some more plots for reasons of comparison

--- R/measuresOfAccuracy.R
+++ R/measuresOfAccuracy.R
@@ -12,62 +12,74 @@ models <- c("oOH", "oOH2", "oOH+oOH2", "oOH+tmax", "oOH+tavg",
           "oOH+prcp", "oOH+tmax+prcp", "oOH+out+prcp", "oOH:out2:prcpvsI",
           "oOH*out", "oOH*tmax2", "oOH*tmax*prcp", "logoOH+logtmax")
 
-#weekdays <- c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Sun_2week_lag")
-weekdays <- c("Sun", "Sun_2week_lag")
+lags <- c("cOI", "cOI_1weekbefore", "cOI_2weeksbefore", "cOI_3weeksbefore", "cOI_4weeksbefore")
 
+accuracy_measures <- data.frame(matrix(ncol = 6, nrow = 0))
+colnames(accuracy_measures) <- c("Model", "lag", "Fstatistic", "RSE", "Rsquared", "AdjRSquared")
 
-accuracy_measures <- data.frame(matrix(ncol = 7, nrow = 0))
-colnames(accuracy_measures) <- c("Model", "Weekday", "LagDurationChangeOfIncidence", "Fstatistic", "RSE", "Rsquared", "AdjRSquared")
-
-#Iterating through all our models and weekdays to extract the following measures of model accuracy
-#F-statistic
-#RSE
-#R Squared
-#Adjusted RSquared
+#Iterating through all our models and lags to extract the following measures of model accuracy
+    #F-statistic
+    #RSE
+    #R Squared
+    #Adjusted RSquared
 
 for (model in models) {
-    for (weekday in weekdays) {
+    for (lag in lags) {
 
-accuracy_measures[nrow(accuracy_measures)+1, 1] <- model
-accuracy_measures[nrow(accuracy_measures), 2] <- weekday
-if (weekday == "Mon") {
-accuracy_measures[nrow(accuracy_measures), 3] <- "13 Day lag"
-} else if (weekday == "Tue") {
-accuracy_measures[nrow(accuracy_measures), 3] <- "12 Day lag"
-} else if (weekday == "Wed") {
-accuracy_measures[nrow(accuracy_measures), 3] <- "11 Day lag"
-} else if (weekday == "Thu") {
-accuracy_measures[nrow(accuracy_measures), 3] <- "10 Day lag"
-} else if (weekday == "Fri") {
-accuracy_measures[nrow(accuracy_measures), 3] <- "9 Day lag"
-} else if (weekday == "Sat") {
-accuracy_measures[nrow(accuracy_measures), 3] <- "8 Day lag"
-} else if (weekday == "Sun") {
-accuracy_measures[nrow(accuracy_measures), 3] <- "7 Day lag"
-} else if (weekday == "Sun_2week_lag") {
-accuracy_measures[nrow(accuracy_measures), 3] <- "14 Day lag"
-}
-wording <- paste0("Regression_", model, weekday)
+accuracy_measures[nrow(accuracy_measures) + 1, 1] <- model
+accuracy_measures[nrow(accuracy_measures), 2] <- lag
 
 #F-Statistic -> needs to be >> 1
-accuracy_measures[nrow(accuracy_measures), 4] <- summary(resultsList[[model]][[weekday]][["Model"]])$fstatistic[1]
+accuracy_measures[nrow(accuracy_measures), 3] <- summary(resultsList[[model]][[lag]][["Model"]])$fstatistic[1]
 
 #RSE -> needs to be small
-accuracy_measures[nrow(accuracy_measures), 5] <- summary(resultsList[[model]][[weekday]][["Model"]])$sigma
+accuracy_measures[nrow(accuracy_measures), 4] <- summary(resultsList[[model]][[lag]][["Model"]])$sigma
 
 #R^2 -> needs to be close to 1
-accuracy_measures[nrow(accuracy_measures), 6] <- summary(resultsList[[model]][[weekday]][["Model"]])$r.squared
+accuracy_measures[nrow(accuracy_measures), 5] <- summary(resultsList[[model]][[lag]][["Model"]])$r.squared
 
 #Adj. R^2 -> needs to be close to 1, accounts for the fact that more variables -> better fit
-accuracy_measures[nrow(accuracy_measures), 7] <- summary(resultsList[[model]][[weekday]][["Model"]])$adj.r.squared
+accuracy_measures[nrow(accuracy_measures), 6] <- summary(resultsList[[model]][[lag]][["Model"]])$adj.r.squared
 
     }
 }
 
-#Visualizations of these measures of accuracy
-ggplot(accuracy_measures, aes(x = LagDurationChangeOfIncidence, fill=LagDurationChangeOfIncidence, y=AdjRSquared)) + 
-    geom_bar(position="dodge", stat="identity") +
+colnames(accuracy_measures) <- c("Model", "lag", "Fstatistic", "RSE", "Rsquared", "AdjRSquared")
+
+#Visualization of adjusted R^2 for different models
+ggplot(accuracy_measures, aes(x = lag, fill = lag, y = AdjRSquared)) +
+    geom_bar(position = "dodge", stat = "identity") +
+    ggtitle("Adjusted R Squared for different models") +
     facet_wrap(~Model) +
-    theme_bw() + 
+    theme_bw() +
     xlab("") +
-    theme(legend.position = "bottom")
+    theme(legend.position = "bottom") #For all models: Maximum reached when considering a 2 week lag
+
+#Visualization of RSE for different models
+ggplot(accuracy_measures, aes(x = lag, fill = lag, y = RSE)) +
+    geom_bar(position = "dodge", stat = "identity") +
+    ggtitle("Adjusted R Squared for different models") +
+    facet_wrap(~Model) +
+    theme_bw() +
+    xlab("") +
+    theme(legend.position = "bottom") #For all models: Minimum reached when considering a 2 week lag
+
+#Visualization of F-Statistic for different models
+ggplot(accuracy_measures, aes(x = lag, fill = lag, y = Fstatistic)) +
+    geom_bar(position = "dodge", stat = "identity") +
+    ggtitle("Adjusted R Squared for different models") +
+    facet_wrap(~Model) +
+    theme_bw() +
+    xlab("") +
+    theme(legend.position = "bottom") #For most models: Maximum reached when considering a 2 week lag, exceptions: oOH, oOH2
+
+
+#Judging from the plots above, a 2 week lag seems to be optimal. Hence, let us create a bar chart of the different models, comparing their adj R^2 for 2 week lag
+accuracy_measures %>% filter(lag == "cOI_2weeksbefore") %>%
+    ggplot(aes(x = fct_reorder(Model, AdjRSquared), y = AdjRSquared)) +
+    geom_bar(stat = "identity", fill = "plum2") +
+    coord_flip() +
+    theme_minimal() +
+    xlab("Model") +
+    ylab("Adjusted R^2") +
+    ggtitle("Comparison Of Adj R^2 for different models (considering a 2 week lag)")
