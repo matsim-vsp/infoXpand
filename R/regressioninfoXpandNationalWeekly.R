57f648c43d5a3d4d370832c0056726698155b06a
Sydpaltra <paltra@tu-berlin.de>
2023-05-16 17:27:28 +0200

    Further work variable selection

--- R/regressioninfoXpandNationalWeekly.R
+++ R/regressioninfoXpandNationalWeekly.R
@@ -53,6 +53,7 @@ resultsList[["oOH2+out"]] <- list()
 resultsList[["oOH2+out2"]] <- list()
 resultsList[["oOH+oOH2+out"]] <- list()
 resultsList[["oOH+oOH2+out2"]] <- list()
+resultsList[["oOH+oOH2+out2_0Intercept"]] <- list()
 resultsList[["oOH*out"]] <- list()
 resultsList[["oOH*out2"]] <- list()
 resultsList[["oOH+prcp"]] <- list()
@@ -73,7 +74,13 @@ resultsList[["oOH*tavg*prcp"]] <- list()
 resultsList[["oOH*out*prcp"]] <- list()
 resultsList[["oOH*out2*prcp"]] <- list()
 resultsList[["logoOH+logtmax"]] <- list()
-
+resultsList[["polyoOH+polyout2"]] <- list()
+resultsList[["oOH*out2+oOH2"]] <- list()
+resultsList[["oOH+oOH2*out2"]] <- list()
+resultslist[["polyoOH3+polyout23"]] <- list()
+resultslist[["polyoOH2+polyout23"]] <- list()
+resultslist[["polyoOH2+out2+out23"]] <- list()
+resultslist[["oOH2+out2_noInt"]] <- list()
 
 ids <- c("oOH", "oOH2", "oOH+oOH2", "oOH+tmax", "oOH+tavg",
           "oOH*tmax", "oOH*tavg", "oOH+oOH2+out", "oOH+oOH2+out2", "oOH+out", "oOH+out2", "oOH2+out", "oOH2+out2", "oOH*out", "oOH*out2",
@@ -82,12 +89,14 @@ ids <- c("oOH", "oOH2", "oOH+oOH2", "oOH+tmax", "oOH+tavg",
           "oOH2*out", "oOH2*out2", "oOH*tmax2", "oOH*tavg2",
           "oOH*tmax*prcp", "oOH*tavg*prcp", "oOH*out*prcp", "oOH*out2*prcp", "logoOH+logtmax",
           "c_oOH", "c_oOH+c_tmax", "c_oOH+c_tavg", "c_oOH+c_out", "c_oOH+c_out2",
-          "c_oOH*c_tmax", "c_oOH*c_tavg", "c_oOH*c_out", "c_oOH*c_out2", "oOH+oOH2+out2")
+          "c_oOH*c_tmax", "c_oOH*c_tavg", "c_oOH*c_out", "c_oOH*c_out2", "oOH+oOH2+out2", "oOH+oOH2+out2_0Intercept", "oOH+oOH2+out2+gradtmax",
+          "polyoOH+polyout2", "oOH*out2+oOH2", "oOH+oOH2*out2", "polyoOH3+polyout23", "polyoOH2+polyout23", "polyoOH2+out2+out23", "oOH2+out2_noInt")
 
 lags <- c("cOI", "cOI_1weekbefore", "cOI_2weeksbefore", "cOI_3weeksbefore", "cOI_4weeksbefore", "cOI_5weeksbefore")
 
 joinedDataFrame <- joinedDataFrame %>%
         mutate(outOfHomeDurationSquared = outOfHomeDuration*outOfHomeDuration) %>%
+        mutate(outdoorFraction2Squared = outdoorFraction2*outdoorFraction2) %>%
         mutate(tmaxSquared = tmax * tmax) %>%
         mutate(tavgSquared = tavg * tavg) %>%
         mutate(logOutOfHomeDuration = log(outOfHomeDuration)) %>%
@@ -98,24 +107,30 @@ joinedDataFrame <- joinedDataFrame %>%
         mutate(c_tmax = tmax - mean(tmax)) %>%
         mutate(c_tavg = tavg - mean(tavg)) %>%
         mutate(c_outdoorFraction = outdoorFraction - mean(outdoorFraction)) %>%
-        mutate(c_outdoorFraction2 = outdoorFraction2 - mean(outdoorFraction2))
+        mutate(c_outdoorFraction2 = outdoorFraction2 - mean(outdoorFraction2)) %>%
+        mutate(stand_outOfHome = scale(outOfHomeDuration),
+              stand_outOfHomeSquared = scale(outOfHomeDurationSquared),
+              stand_tmax = scale(tmax)) %>%
+        mutate(centDiffquot_tmax = (lead(tmax)-lag(tmax))/2) %>%
+        mutate(outdoorFraction2Cubed = outdoorFraction2 *outdoorFraction2 * outdoorFraction2)
+
 
 # joinedDataFrame <- joinedDataFrame[complete.cases(joinedDataFrame), ]
 
 for (id in ids) {
   for (lag in lags) {
   if (lag == "cOI") {
-    joinedDataFrame <- joinedDataFrame %>% filter(cOI < 2)
+    joinedDataFrame_reduced <- joinedDataFrame %>% filter(cOI < 2)
   } else if (lag == "cOI_1weekbefore") {
-    joinedDataFrame <- joinedDataFrame %>% filter(cOI_1weekbefore < 2)
+    joinedDataFrame_reduced <- joinedDataFrame %>% filter(cOI_1weekbefore < 2)
   } else if (lag == "cOI_2weeksbefore") {
-    joinedDataFrame <- joinedDataFrame %>% filter(cOI_2weeksbefore < 2)
+    joinedDataFrame_reduced <- joinedDataFrame %>% filter(cOI_2weeksbefore < 2)
   } else if (lag == "cOI_3weeksbefore") {
-    joinedDataFrame <- joinedDataFrame %>% filter(cOI_3weeksbefore < 2)
+    joinedDataFrame_reduced <- joinedDataFrame %>% filter(cOI_3weeksbefore < 2)
   } else if (lag == "cOI_4weeksbefore") {
-    joinedDataFrame <- joinedDataFrame %>% filter(cOI_4weeksbefore < 2)
+    joinedDataFrame_reduced <- joinedDataFrame %>% filter(cOI_4weeksbefore < 2)
   } else if (lag == "cOI_5weeksbefore") {
-    joinedDataFrame <- joinedDataFrame %>% filter(cOI_5weeksbefore < 2)
+    joinedDataFrame_reduced <- joinedDataFrame %>% filter(cOI_5weeksbefore < 2)
   }
   resultsList[[id]][[lag]] <- list()
   if (id == "oOH") {
@@ -140,6 +155,8 @@ for (id in ids) {
     formula.lm <- paste0(lag, " ~ outOfHomeDurationSquared + outdoorFraction")
   } else if (id == "oOH2+out2") {
     formula.lm <- paste0(lag, " ~ outOfHomeDurationSquared + outdoorFraction2")
+  } else if (id == "oOH2+out2_noInt") {
+    formula.lm <- paste0(lag, " ~ 0+ outOfHomeDurationSquared + outdoorFraction2")
   } else if (id == "oOH+oOH2+out") {
     formula.lm <- paste0(lag, " ~ outOfHomeDuration + outOfHomeDurationSquared + outdoorFraction")
   } else if (id == "oOH+oOH2+out2") {
@@ -204,28 +221,53 @@ for (id in ids) {
     formula.lm <- paste0(lag, " ~ c_outOfHomeDuration * c_outdoorFraction2")
   }  else if (id == "oOH+oOH2+out2") {
     formula.lm <- paste0(lag, " ~ outOfHomeDuration + outOfHomeDurationSquared + outdoorFraction2")
+  }  else if (id == "oOH+oOH2+out2_0Intercept") {
+    formula.lm <- paste0(lag, " ~ 0 + outOfHomeDurationSquared + outdoorFraction2")
+  } else if (id == "oOH+oOH2+out2+gradtmax") {
+    formula.lm <- paste0(lag, " ~ outOfHomeDuration + outOfHomeDurationSquared + outdoorFraction2 + centDiffquot_tmax")
+    joinedDataFrame_reduced <- joinedDataFrame_reduced[-1,]
+    joinedDataFrame_reduced <- joinedDataFrame_reduced[-nrow(joinedDataFrame_reduced),]
+  } else if (id == "polyoOH+polyout2") {
+    formula.lm <- paste0(lag, " ~ poly(outOfHomeDuration,2)+poly(outdoorFraction2,2)")
+  } else if (id == "oOH*out2+oOH2") {
+    formula.lm <- paste0(lag, " ~ outOfHomeDuration*outdoorFraction2 + outOfHomeDurationSquared")
+  } else if (id == "oOH+oOH2*out2") {
+    formula.lm <- paste0(lag, " ~ outOfHomeDuration + outOfHomeDurationSquared*outdoorFraction2")
+  } else if (id == "polyoOH3+polyout23") {
+    formula.lm <- paste0(lag, " ~ poly(outOfHomeDuration,3) + poly(outdoorFraction2,3)")
+  } else if (id == "polyoOH2+polyout23") {
+    formula.lm <- paste0(lag, " ~ poly(outOfHomeDuration,2) + poly(outdoorFraction2,3)")
+  } else if (id == "polyoOH2+out2+out23") {
+    formula.lm <- paste0(lag, " ~ poly(outOfHomeDuration,2) + outdoorFraction2 + outdoorFraction2Cubed")
   }
 
-    model.lm <- lm(formula = formula.lm, data = joinedDataFrame) # Regression
+    model.lm <- lm(formula = formula.lm, data = joinedDataFrame_reduced) # Regression
     resultsList[[id]][[lag]][["Model"]] <- model.lm
     resultsList[[id]][[lag]][["AIC"]] <- AIC(model.lm)
     resultsList[[id]][[lag]][["BIC"]] <- BIC(model.lm)
 
     #Leave one out cross validation
     train.control <- trainControl(method = "LOOCV")
-    model.loocv <- train(eval(parse(text=formula.lm)), data = joinedDataFrame, method = "lm", trControl = train.control)
+    model.loocv <- train(eval(parse(text=formula.lm)), data = joinedDataFrame_reduced, method = "lm", trControl = train.control)
     resultsList[[id]][[lag]][["Model_LOOCV"]] <- model.loocv
 
-    resultsList[[id]][[lag]][["PlotActualFitted"]] <- ggplot(data = joinedDataFrame) + 
+    #10-fold cross validation
+    train.control <- trainControl(method = "cv", number = 10)
+    model.10foldcv <- train(eval(parse(text=formula.lm)), data = joinedDataFrame_reduced, method = "lm", trControl = train.control)
+    resultsList[[id]][[lag]][["Model_10foldCV"]] <- model.10foldcv
+
+    joinedDataFrame_reduced$labeled <- ifelse(joinedDataFrame_reduced[[lag]] < 1.5, "", as.character(joinedDataFrame_reduced$Date))
+    resultsList[[id]][[lag]][["PlotActualFitted"]] <- ggplot(data = joinedDataFrame_reduced) + 
     geom_smooth(aes(x=predict(model.lm), y = .data[[lag]], color="#4b4b4b"), method="lm", size = 2) +
+    geom_text(aes(x = predict(model.lm), y = .data[[lag]], label=labeled),hjust=1.2, vjust=0.5) +
     xlab("Predicted Values") +
     ylab("Observed Values") +
-    #ggtitle(paste0("lag: ", lag, ", Model: ", id)) +
+    ggtitle(paste0("lag: ", lag, ", Model: ", id)) +
     #geom_abline(aes(intercept = 0, slope = 1, color = "blue")) +
     scale_color_identity(labels = c("Regression line"), guide = "legend") +
-    theme_minimal() +
-    theme(legend.position = "bottom", legend.title = element_blank()) +
-   geom_point(data = joinedDataFrame, aes(x = predict(model.lm), y = .data[[lag]], fill=Welle), size=3, shape=21)
+    theme_minimal(base_size = 12) +
+    theme(legend.position = "bottom", legend.title = element_blank(), legend.text=element_text(size=12)) +
+   geom_point(data = joinedDataFrame_reduced, aes(x = predict(model.lm), y = .data[[lag]], fill=Welle), size=3, shape=21)
 
 
     resultsList[[id]][[lag]][["ResvsFitted"]] <- function() {
@@ -240,65 +282,58 @@ for (id in ids) {
     resultsList[[id]][[lag]][["Cooksdist"]] <- function() {
     plot(model.lm, which = 4)
     }
+
   }
 }
 
-summary(resultsList[["oOH"]][["cOI"]][["Model"]])
-summary(resultsList[["oOH"]][["cOI_1weekbefore"]][["Model"]])
-summary(resultsList[["oOH"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["oOH*out2"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["oOH"]][["cOI_3weeksbefore"]][["Model"]])
-summary(resultsList[["oOH"]][["cOI_4weeksbefore"]][["Model"]])
-summary(resultsList[["oOH"]][["cOI_5weeksbefore"]][["Model"]])
+#Stepwise Model selection
+#Step 1
+model_step1 <- resultsList[["polyoOH3+polyout23"]][["cOI_2weeksbefore"]][["Model"]]
+AIC(model_step1)
+BIC(model_step1)
+summary(resultsList[["polyoOH3+polyout23"]][["cOI_2weeksbefore"]][["Model"]])
+confint(resultsList[["polyoOH3+polyout23"]][["cOI_2weeksbefore"]][["Model"]])
+#Step2
+model_step2 <- resultsList[["polyoOH2+polyout23"]][["cOI_2weeksbefore"]][["Model"]]
+AIC(model_step2)
+BIC(model_step2)
+summary(resultsList[["polyoOH2+polyout23"]][["cOI_2weeksbefore"]][["Model"]])
+confint(resultsList[["polyoOH2+polyout23"]][["cOI_2weeksbefore"]][["Model"]])
 
-summary(resultsList[["oOH+tmax"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["oOH+tavg"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["oOH+out"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["oOH+out2"]][["cOI_2weeksbefore"]][["Model"]])
+#Step3
+model_step3 <- resultsList[["polyoOH2+out2+out23"]][["cOI_2weeksbefore"]][["Model"]]
+AIC(model_step3)
+BIC(model_step3)
+summary(resultsList[["polyoOH2+out2+out23"]][["cOI_2weeksbefore"]][["Model"]])
+confint(resultsList[["polyoOH2+out2+out23"]][["cOI_2weeksbefore"]][["Model"]])
 
-summary(resultsList[["oOH2+out"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["oOH2+out2"]][["cOI_2weeksbefore"]][["Model"]])
-
-
-summary(resultsList[["oOH+oOH2+out"]][["cOI_2weeksbefore"]][["Model"]])
+#Step4 
+model_step4 <- resultsList[["oOH+oOH2+out2"]][["cOI_2weeksbefore"]][["Model"]]
+AIC(model_step4)
+BIC(model_step4)
 summary(resultsList[["oOH+oOH2+out2"]][["cOI_2weeksbefore"]][["Model"]])
+confint(resultsList[["oOH+oOH2+out2"]][["cOI_2weeksbefore"]][["Model"]])
 
-summary(resultsList[["oOH*out"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["oOH*out2"]][["cOI_2weeksbefore"]][["Model"]])
+#Step5
+model_step5 <- resultsList[["oOH2+out2"]][["cOI_2weeksbefore"]][["Model"]]
+AIC(model_step5)
+BIC(model_step5)
+summary(resultsList[["oOH2+out2"]][["cOI_2weeksbefore"]][["Model"]])
+confint(resultsList[["oOH2+out2"]][["cOI_2weeksbefore"]][["Model"]])
 
-summary(resultsList[["c_oOH"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["c_oOH+c_tmax"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["c_oOH+c_tavg"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["c_oOH+c_out"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["c_oOH+c_out2"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["c_oOH*c_tmax"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["c_oOH*c_tavg"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["c_oOH*c_out"]][["cOI_2weeksbefore"]][["Model"]])
-summary(resultsList[["c_oOH*c_out2"]][["cOI_2weeksbefore"]][["Model"]])
-
-resultsList[["oOH+oOH2+out2"]][["cOI_2weeksbefore"]][["PlotActualFitted"]]
-resultsList[["oOH+out"]][["cOI_2weeksbefore"]][["Qqplot"]]()
-resultsList[["oOH+out"]][["cOI_2weeksbefore"]][["ScaleLoc"]]()
-resultsList[["oOH+out"]][["cOI_2weeksbefore"]][["Cooksdist"]]()
-
-grid.arrange(resultsList[["oOH"]][["cOI"]][["PlotActualFitted"]], resultsList[["oOH"]][["cOI_1weekbefore"]][["PlotActualFitted"]],
-resultsList[["oOH"]][["cOI_2weeksbefore"]][["PlotActualFitted"]], resultsList[["oOH"]][["cOI_3weeksbefore"]][["PlotActualFitted"]], nrow=2)
-
-grid.arrange(resultsList[["oOH*out"]][["cOI"]][["PlotActualFitted"]], resultsList[["oOH*out"]][["cOI_1weekbefore"]][["PlotActualFitted"]],
-resultsList[["oOH*out"]][["cOI_2weeksbefore"]][["PlotActualFitted"]], resultsList[["oOH*out"]][["cOI_3weeksbefore"]][["PlotActualFitted"]], nrow=2)
-
-g <- arrangeGrob(resultsList[["oOH*out"]][["cOI"]][["PlotActualFitted"]], resultsList[["oOH*out"]][["cOI_1weekbefore"]][["PlotActualFitted"]],
-resultsList[["oOH*out"]][["cOI_2weeksbefore"]][["PlotActualFitted"]], resultsList[["oOH*out"]][["cOI_3weeksbefore"]][["PlotActualFitted"]], nrow=2)
+#Step6
+model_step6 <- resultsList[["oOH2+out2_noInt"]][["cOI_2weeksbefore"]][["Model"]]
+AIC(model_step6)
+BIC(model_step6)
+summary(resultsList[["oOH2+out2_noInt"]][["cOI_2weeksbefore"]][["Model"]])
+confint(resultsList[["oOH2+out2_noInt"]][["cOI_2weeksbefore"]][["Model"]])
 
 
-grid.arrange(resultsList[["oOH*tmax"]][["cOI"]][["PlotActualFitted"]], resultsList[["oOH*tmax"]][["cOI_1weekbefore"]][["PlotActualFitted"]],
-resultsList[["oOH*tmax"]][["cOI_2weeksbefore"]][["PlotActualFitted"]], resultsList[["oOH*tmax"]][["cOI_3weeksbefore"]][["PlotActualFitted"]], nrow=2)
 
 
-g <- arrangeGrob(resultsList[["oOH"]][["cOI"]][["PlotActualFitted"]], resultsList[["oOH"]][["cOI_1weekbefore"]][["PlotActualFitted"]],
-resultsList[["oOH"]][["cOI_2weeksbefore"]][["PlotActualFitted"]], resultsList[["oOH"]][["cOI_3weeksbefore"]][["PlotActualFitted"]], nrow=2)
 
-#We have decided on the model oOH+out2
+
+#We have decided on the model oOH+oOH2+out2
 #We now check if the relationship changes over time
 #We therefore consider the following 3 models
 # (1) Using only data from the 1st wave
