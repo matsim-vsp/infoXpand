d8b4253b26dfa04295b30ea6614e52d0358b1043
Sydpaltra <paltra@tu-berlin.de>
2023-07-18 16:59:25 +0200

    Man Comp of adj r2 for no-intercept models

--- R/adjustedR2.R
+++ R/adjustedR2.R
@@ -2,31 +2,31 @@ library(tidyverse)
 
 #Computation of r2 for no-intercept models
 r2 <- function(data, lag, model){
- if(lag == "cOI") {
-    data <- data %>% filter(cOI < 2) %>% filter(Date < "2021-01-01")
- }
-  if(lag == "cOI_1weekbefore") {
-    data <- data %>% filter(cOI_1weekbefore < 2) %>% filter(Date + 7 < "2021-01-01")
- }
-  if(lag == "cOI_2weeksbefore") {
-    data <- data %>% filter(cOI_2weeksbefore < 2) %>% filter(Date + 14 < "2021-01-01")
- }
+    data <- data %>% filter(Date > "2020-03-08") %>% filter(Date < "2020-12-20")
  1 - sum((data[[lag]] - predict(resultsList[[model]][[lag]][["Model"]]))^2)/sum((data[[lag]] - mean(data[[lag]]))^2)
 }
 
+#Computation of r2 for chosen model
 r2_weather_noInt_0weeks <- r2(joinedDataFrame, "cOI", "oOH2+oOH2:out2_noInt")
 r2_weather_noInt_1week <- r2(joinedDataFrame, "cOI_1weekbefore", "oOH2+oOH2:out2_noInt")
 r2_weather_noInt_2weeks <- r2(joinedDataFrame, "cOI_2weeksbefore", "oOH2+oOH2:out2_noInt")
 r2_weather_noInt_3weeks <- r2(joinedDataFrame, "cOI_3weeksbefore", "oOH2+oOH2:out2_noInt")
 r2_weather_noInt_4weeks <- r2(joinedDataFrame, "cOI_4weeksbefore", "oOH2+oOH2:out2_noInt")
+#Computation of r2 for models using tmax/tavg
+r2_tmax_noInt_2weeks <- r2(joinedDataFrame, "cOI_2weeksbefore", "oOH2+oOH2:tmax_noInt")
+r2_tavg_noInt_2weeks <- r2(joinedDataFrame, "cOI_2weeksbefore", "oOH2+oOH2:tavg_noInt")
 
 #Computation of adjusted r2 for no-intercept models
 adjustedr2 <- function(r2, n, p){
  1 - ((1-r2)*(n-1))/(n-p)
 }
 
-adjr2_weather_noInt <- adjustedr2(r2_weather_noInt, 40, 2)
-adjr2_weather <- adjustedr2(r2_weather, 40, 2)
-adjr2_noweather_noInt <- adjustedr2(r2_noweather_noInt, 40, 1)
-adjr2_noweather <- adjustedr2(r2_noweather, 40, 1)
-
+#Computation of adjusted r2 for chosen model
+adjr2_weather_noInt_0weeks <- adjustedr2(r2_weather_noInt_0weeks, 40, 2)
+adjr2_weather_noInt_1week <- adjustedr2(r2_weather_noInt_1week, 40, 2)
+adjr2_weather_noInt_2weeks <- adjustedr2(r2_weather_noInt_2weeks, 40, 2)
+adjr2_weather_noInt_3weeks <- adjustedr2(r2_weather_noInt_3weeks, 40, 2)
+adjr2_weather_noInt_4weeks <- adjustedr2(r2_weather_noInt_4weeks, 40, 2)
+#Computation of adjusted r2 for models using tmax/tavg
+adjr2_tmax_noInt_2weeks <- adjustedr2(r2_tmax_noInt_2weeks, 40, 2)
+adjr2_tavg_noInt_2weeks <- adjustedr2(r2_tavg_noInt_2weeks, 40, 2)
