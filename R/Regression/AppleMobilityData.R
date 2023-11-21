79e432732bb5850419a93fbbb749c73606d8e1ed
Sydpaltra <paltra@tu-berlin.de>
2023-11-21 14:37:38 +0100

    Update of plots, cleaning

---
+++ R/Regression/AppleMobilityData.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(lubridate)
+
+# In this script we are using mobility data which was collected and provided by Apple from April 2020 until April 2022.
+# As the data is no longer provided on Apple's website, we are obtaining the data from https://github.com/ActiveConclusion/COVID19_mobility
+
+#Reading in the data, filtering for Germany and removing the first date as this is representing baseline
+apple_mobility_data <- read_csv("https://raw.githubusercontent.com/ActiveConclusion/COVID19_mobility/master/apple_reports/apple_mobility_report.csv") %>%
+filter(country == "Germany") %>%
+filter(date != min(date))
+
+#Computing weekly averages
+apple_mobility_data <- apple_mobility_data %>%
+    mutate(week = week(date)) %>%
+    mutate(year = year(date)) %>%
+    group_by(`sub-region`, week, year) %>%
+    summarise(date = max(date), weekly_change_driving = mean(driving), weekly_change_transit = mean(transit, na.rm=TRUE), weekly_change_walking = mean(walking))
+
+apple_mobility_data <- apple_mobility_data %>% mutate(date = as.Date(date))
+
+#Plotting the weekly changes for the different federal states
+    cols <- c("driving" = "red", "transit" = "purple", "walking" = "blue")
+    ggplot(data = apple_mobility_data) +
+    geom_line(aes(x = date, y = weekly_change_driving, color = "driving")) +
+    geom_line(aes(x = date, y = weekly_change_transit, color = "transit")) +
+    geom_line(aes(x = date, y = weekly_change_walking, color = "walking")) +
+    scale_color_manual(values = cols) +
+    labs(title = "Weekly average change in % compared to baseline (January 13th, 2020)",
+        x = "Date",
+        y = "Change in %") +
+    theme_minimal() +
+    theme(legend.position = "bottom", axis.text.x = element_text(angle=45, hjust = 1), legend.title = element_blank()) +
+    facet_wrap(vars(`sub-region`)) +
+    scale_x_date(breaks = "6 months")
---
+++ R/Regression/GoogleMobilityData.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(lubridate)
+
+#Read in Google mobility data
+google_mobility_data <- read_csv("https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv", col_select=c("sub_region_1", "country_region_code", "country_region", "date", "retail_and_recreation_percent_change_from_baseline", "grocery_and_pharmacy_percent_change_from_baseline", "parks_percent_change_from_baseline", "transit_stations_percent_change_from_baseline", "workplaces_percent_change_from_baseline", "residential_percent_change_from_baseline"))
+
+#Filter for Germany
+google_mobility_data <- filter(google_mobility_data, country_region == "Germany")
+google_mobility_data <- google_mobility_data %>% mutate(sub_region_1 = ifelse(is.na(sub_region_1), "Germany", sub_region_1))
+
+#Data is provided on a daily level, but we look at it on a weekly level
+google_mobility_data <- mutate(google_mobility_data, year = year(date))
+google_mobility_data <- mutate(google_mobility_data, week = isoweek(date))
+google_mobility_data_weekly <- google_mobility_data %>% group_by(week, year, sub_region_1) %>%
+  summarise(date = max(date),
+            retail_and_recreation = mean(retail_and_recreation_percent_change_from_baseline, na.rm = TRUE),
+            grocery_and_pharmacy = mean(grocery_and_pharmacy_percent_change_from_baseline, na.rm = TRUE),
+            parks = mean(parks_percent_change_from_baseline, na.rm = TRUE),
+            transit_stations = mean(transit_stations_percent_change_from_baseline, na.rm = TRUE),
+            workplaces = mean(workplaces_percent_change_from_baseline, na.rm = TRUE),
+            residential = mean(residential_percent_change_from_baseline, na.rm = TRUE))
+
+google_mobility_data_weekly <- google_mobility_data_weekly %>% filter(sub_region_1 == "Germany") %>% filter(date > "2020-03-01") %>% filter(date < "2020-12-31")
+
+#Turning the data into tidy format
+google_mobility_data_weekly <- pivot_longer(google_mobility_data_weekly , cols = c("retail_and_recreation", "grocery_and_pharmacy", "parks", "transit_stations", "workplaces", "residential"), names_to="category", values_to = "changeFromBaseline")
+
+#Plotting the data for 2021 for all 16 federal German states and for all of Germany
+google_mobility_data_weekly %>% filter(date < "2021-01-01") %>% # filter(category!="parks") %>%
+ggplot() +
+  geom_point(mapping = aes(x = date, y = changeFromBaseline, col = category)) +
+  theme_minimal() +
+  theme(legend.title = element_blank()) +
+  theme(legend.position = "bottom") +
+  ylab("Percentage Change From Baseline") +
+  xlab("Date") +
+  facet_wrap(vars(sub_region_1))
